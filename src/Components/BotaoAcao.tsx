2a85f0dd9a130c23795d2a5c41aaeb3fc88fe6c2
yasmin <yasminsrmd@gmail.com>
2025-05-15 21:14:57 -0300

    mudança background, adição de hover no botão e mudança no tamanho das letras

--- src/Components/BotaoAcao.tsx
+++ src/Components/BotaoAcao.tsx
@@ -1,24 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 interface BotaoAcaoProps{
     comando: string;
     className?: string;
     funcao?: (e: React.FormEvent<Element>) => void | Promise<void>;
 }
+
+const StyledButton = styled.button`
+  background-color: #007bff;
+  color: white;
+  border: none;
+  height: 60px;
+  width: 200px;
+  border-radius: 10px;
+  font-size: 1rem;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #0056d2;
+  }
+`;
+
 const BotaoAcao: React.FC<BotaoAcaoProps> = props =>{
-    return(<button onClick = {props.funcao} className="btn btn-primary col-12 mx-auto" style={styles.btnCustom}>{props.comando}</button>);
+    return <StyledButton onClick={props.funcao}>{props.comando}</StyledButton>;
 }
-const styles = {
-    btnCustom: {
-        marginTop: "40px",
-        background: "#007bff", 
-        color: "white",
-        border: "none",
-        height: "60px",
-        width: "200px",
-        borderRadius: "10px",
-        fontSize: "1rem",
-        fontWeight: "bold"
-      }
-};
+
+    
    
 export default BotaoAcao;
